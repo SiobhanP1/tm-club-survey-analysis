c243de05ccf0bcf562f63a0d423ecbdf4b01498a
SiobhanP1 <siobhanphillips11@gmail.com>
2023-03-10 12:41:27 +0000

    Add survey data parameter to club class

--- run.py
+++ run.py
@@ -16,41 +16,34 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('DublinTMclubSurveyData')
 
 worksheet = SHEET.get_worksheet(0)
-all_data = worksheet.get_all_values()
-pprint(all_data)
+dublin_data = worksheet.get_all_values()
 
 
 class Club:
     """
     Toastmaster club class
     """
-    def __init__(self, club_name, num_members, club_type, meetings_per_month):
+    def __init__(self, club_name, num_members, club_type, meetings_per_month, data):
         self.club_name = club_name
         self.num_members = num_members
         self.club_type = club_type
         self.meetings_per_month = meetings_per_month
-   
+        self.data = data
 
     def club_description(self):
         """
-        Returns description of instance of club class. 
+        Returns description of instance of club class.
         """
         return f"{self.club_name} Toastmasters Club is a {self.club_type} club with {self.meetings_per_month} meetings a month. There are {self.num_members} members."
-    
-
-    def calc_meetings_per_year(self):
-        """
-        Calculates the number of club meetings per year. 
-        """
-        return self.meetings_per_month * 12
 
 
 def main():
     """
     Main function called when user clicks 'Run Program'.
     """
-    dublin_club = Club('Dublin', 22, 'regular', 4)
+    dublin_club = Club('Dublin', 22, 'regular', 4, dublin_data)
     print(dublin_club.club_description())
+    pprint(dublin_data)
 
 
 main()
